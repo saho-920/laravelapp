bd5dc87591f3c6aeabb974f273cbca516ba5796f
saho-920 <sevenstar9@icloud.com>
2022-06-17 13:50:16 +0900

    テキストP72まで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,14 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
    class HelloController extends Controller
    {
-     
-    public function index()
-{
-   $data = ['msg'=>'これはコントローラから渡されたメッセージです。'];
-   return view('hello.index', $data);
-}
+      
+         public function index()
+         {
+             $data = [
+                 'msg'=>'お名前を入力下さい。',
+             ];
+             return view('hello.index', $data);
+         }
+      
+         public function post(Request $request)
+         {
+             $msg = $request->msg;
+             $data = [
+                 'msg'=>'こんにちは、' . $msg . 'さん！',
+             ];
+             return view('hello.index', $data);
+         }
+      
+      }
+      
+      
 
-   }
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,10 @@ Route::get('/', function () {
 // Route::get('hello', 'HelloController@index');
 // Route::get('hello/other', 'HelloController@other');
 
+// Route::get('hello', 'HelloController@index');
+
 Route::get('hello', 'HelloController@index');
+Route::post('hello', 'HelloController@post');
 
 // ------実習用-------
 Route::get('jissyu', 'JissyuController@index');
