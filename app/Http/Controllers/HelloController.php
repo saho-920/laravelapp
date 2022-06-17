f65c9f6a6124397dbc3a92239433b7445519438b
saho-920 <sevenstar9@icloud.com>
2022-06-17 14:30:12 +0900

    テキストP84まで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,24 +6,16 @@ use Illuminate\Http\Request;
    class HelloController extends Controller
    {
       
-         public function index()
-         {
-             $data = [
-                 'msg'=>'お名前を入力下さい。',
-             ];
-             return view('hello.index', $data);
-         }
-      
-         public function post(Request $request)
-         {
-             $msg = $request->msg;
-             $data = [
-                 'msg'=>'こんにちは、' . $msg . 'さん！',
-             ];
-             return view('hello.index', $data);
-         }
-      
+  
+      public function index()
+      {
+         $data = ['one', 'two', 'three', 'four', 'five'];
+         return view('hello.index', ['data'=>$data]);
       }
+      
+}
+ 
+
       
       
 
