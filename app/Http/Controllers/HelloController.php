451129a5a422f77560b7bb8a4f083ee5fdde6012
saho-920 <sevenstar9@icloud.com>
2022-07-01 12:02:33 +0900

    P193DB設定

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
-  
-   public function index(Request $request)
-   {
-       return view('hello.index', ['msg'=>'フォームを入力下さい。']);
-   }
+
+
+public function index(Request $request)
+{
+   $items = DB::select('select * from people');
+   return view('hello.index', ['items' => $items]);
+}
+
 
    public function post(HelloRequest $request)
    {
