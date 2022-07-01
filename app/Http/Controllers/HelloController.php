2f5f2a8500c9255bf2f91955a631ef24a8673c44
saho-920 <sevenstar9@icloud.com>
2022-07-01 14:35:58 +0900

    P202 DB削除

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class HelloController extends Controller
 {
 
-    // データの表示
+// データの表示
     public function index(Request $request)
     {
        if (isset($request->id))
@@ -24,45 +24,66 @@ class HelloController extends Controller
     }
     
 
-   public function post(HelloRequest $request)
-   {
+    public function post(HelloRequest $request)
+    {
        return view('hello.index', ['msg'=>'正しく入力されました！']);
-   }
-    // データの挿入
-public function add(Request $request)
-{
-    return view('hello.add');
-}
+    }
 
-public function create(Request $request)
-{
+
+// データの挿入
+    public function add(Request $request)
+    {
+        return view('hello.add');
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('/hello');
+    }
+
+
+// データの更新
+    public function edit(Request $request)
+    {
+    $param = ['id' => $request->id];
+    $item = DB::select('select * from people where id = :id', $param);
+    return view('hello.edit', ['form' => $item[0]]);
+    }
+
+    public function update(Request $request)
+    {
     $param = [
+        'id' => $request->id,
         'name' => $request->name,
         'mail' => $request->mail,
         'age' => $request->age,
     ];
-    DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+    DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
     return redirect('/hello');
-}
-// データの更新
-public function edit(Request $request)
-{
-   $param = ['id' => $request->id];
-   $item = DB::select('select * from people where id = :id', $param);
-   return view('hello.edit', ['form' => $item[0]]);
-}
+    }
 
-public function update(Request $request)
-{
-   $param = [
-       'id' => $request->id,
-       'name' => $request->name,
-       'mail' => $request->mail,
-       'age' => $request->age,
-   ];
-   DB::update('update people set name =:name, mail = :mail, age = :age where id = :id', $param);
-   return redirect('/hello');
-}
+
+// データの削除
+    public function del(Request $request)
+    {
+    $param = ['id' => $request->id];
+    $item = DB::select('select * from people where id = :id', $param);
+    return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove(Request $request)
+    {
+    $param = ['id' => $request->id];
+    DB::delete('delete from people where id = :id', $param);
+    return redirect('/hello');
+    }
+
 
 
 }
