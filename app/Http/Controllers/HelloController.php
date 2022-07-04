2a804ec88b16eb32782fe284c70484789659e70b
saho-920 <sevenstar9@icloud.com>
2022-07-04 11:24:15 +0900

    P205 DB::tableとget

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,18 +10,11 @@ class HelloController extends Controller
 {
 
 // データの表示
-    public function index(Request $request)
-    {
-       if (isset($request->id))
-       {
-          $param = ['id' => $request->id];
-          $items = DB::select('select * from people where id = :id',
-             $param);
-       } else {
-          $items = DB::select('select * from people');
-       }
-       return view('hello.index', ['items' => $items]);
-    }
+public function index(Request $request)
+{
+   $items = DB::table('people')->get();
+   return view('hello.index', ['items' => $items]);
+}
     
 
     public function post(HelloRequest $request)
