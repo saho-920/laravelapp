8b8d30d9d4b5203cdf22a24583bfbe5cb5796181
saho-920 <sevenstar9@icloud.com>
2022-07-06 11:51:47 +0900

    P246　whereによる検索

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -20,9 +20,9 @@ class PersonController extends Controller
 
     public function search(Request $request)
     {
-    $item = Person::find($request->input);
-    $param = ['input' => $request->input, 'item' => $item];
-    return view('person.find', $param);
+       $item = Person::where('name', $request->input)->first();
+       $param = ['input' => $request->input, 'item' => $item];
+       return view('person.find', $param);
     }
-
+    
 }
